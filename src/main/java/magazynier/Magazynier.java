e1c8ef63c077d2ef26b57ba1d1738246d36ab8f7
Potat0x <Potat0x@users.noreply.github.com>
2018-04-29 14:56:52 +0200

    Initial from Maven

---
+++ src/main/java/magazynier/Magazynier.java
@@ -0,0 +1,7 @@
+package magazynier;
+
+public class Magazynier {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/magazynier/MagazynierTest.java
@@ -0,0 +1,12 @@
+package magazynier;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MagazynierTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
