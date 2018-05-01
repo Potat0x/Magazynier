2627bd7dc27f976d2b545734332fbee73f430d86
Potat0x <Potat0x@users.noreply.github.com>
2018-05-01 16:42:10 +0200

    Add database connection test

---
+++ src/test/java/magazynier/ConnectionTest.java
@@ -0,0 +1,31 @@
+package magazynier;
+
+import org.junit.Test;
+
+import java.sql.*;
+
+import static junit.framework.TestCase.fail;
+
+public class ConnectionTest {
+    @Test
+    public void connectionTest() {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "magazynier", "m1234");
+
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT sysdate FROM dual");
+
+            if (rs.next()) {
+                //System.out.println(rs.getString(1));
+            } else {
+                fail("select sysdate fail");
+            }
+            rs.close();
+            connection.close();
+        } catch (Exception e) {
+            //e.printStackTrace();
+            fail(e.getClass().getSimpleName());
+        }
+    }
+}
