32ad7b0e52cc8edf6c15800f6ede00bd66082c2d
Potat0x <Potat0x@users.noreply.github.com>
2018-05-04 18:13:37 +0200

    Add contractors updating

--- src/main/java/magazynier/ContractorsController.java
+++ src/main/java/magazynier/ContractorsController.java
@@ -4,15 +4,24 @@ import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.util.Callback;
+import javassist.NotFoundException;
 import magazynier.entities.Contractor;
+import magazynier.utils.AlertLauncher;
+import magazynier.utils.PeselValidator;
+import magazynier.utils.TextFieldOverflowIndicator;
 
 import java.util.ArrayList;
 
+@SuppressWarnings("unchecked")
 public class ContractorsController {
+
+    private final int MAX_COMPANY_NAME_LENGTH = 50;
+    private final int MAX_TEXT_FIELD_LENGTH = 25;
 
     public TextField firstName;
     public TextField lastName;
@@ -31,17 +40,26 @@ public class ContractorsController {
     public TableColumn lastNameCol;
     public TableView contractorsTable;
     public GridPane form;
+    public Button editButton;
+    public Button cancelButton;
+    public Button saveButton;
+    public Button deleteButton;
 
     private ContractorsModel model;
 
+    private boolean contractorAdding;
+    private boolean contractorEditing;
+
     public ContractorsController() {
         model = new ContractorsModel();
+        contractorAdding = false;
+        contractorEditing = false;
     }
 
     private void refreshTable() {
-        ArrayList workers = model.getContractorsList();
+        ArrayList contractors = model.getContractorsList();
         contractorsTable.getItems().clear();
-        contractorsTable.getItems().addAll(workers);
+        contractorsTable.getItems().addAll(contractors);
     }
 
     @FXML
@@ -80,13 +98,178 @@ public class ContractorsController {
             }
         });
 
+        contractorsTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            //deleteButton.setDisable(newValue == null);
+            boolean rowNotSelected = newValue == null;
+            form.setDisable(rowNotSelected);
+            editButton.setDisable(rowNotSelected);
+            deleteButton.setDisable(rowNotSelected);
+//            saveButton.setDisable(rowNotSelected);
+//            cancelButton.setDisable(rowNotSelected);
+        });
+
+        TextFieldOverflowIndicator.set(contractorName, MAX_COMPANY_NAME_LENGTH);
+        TextFieldOverflowIndicator.set(firstName, MAX_TEXT_FIELD_LENGTH);
+        TextFieldOverflowIndicator.set(lastName, MAX_TEXT_FIELD_LENGTH);
+        TextFieldOverflowIndicator.set(phone, MAX_TEXT_FIELD_LENGTH);
+        TextFieldOverflowIndicator.set(email, MAX_TEXT_FIELD_LENGTH);
+        TextFieldOverflowIndicator.set(street, MAX_TEXT_FIELD_LENGTH);
+        TextFieldOverflowIndicator.set(city, MAX_TEXT_FIELD_LENGTH);
+
         refreshTable();
         form.setDisable(true);
     }
 
+    @FXML
+    public void updateForm() {
+
+        Contractor sc = (Contractor) contractorsTable.getSelectionModel().getSelectedItem();
+
+        if (sc != null) {
+            type.getSelectionModel().select(sc.getEntityType());
+            firstName.setText(sc.getFirstName());
+            lastName.setText(sc.getLastName());
+            contractorName.setText(sc.getContractorName());
+            phone.setText(sc.getPhone());
+            email.setText(sc.getEmail());
+            nip.setText(sc.getNip());
+            pesel.setText(sc.getPesel());
+            street.setText(sc.getStreet());
+            city.setText(sc.getCity());
+        }
+/*        saveButton.setDisable(true);
+        cancelButton.setDisable(true);*/
+        //clearFormStyles();//--todo: delete this line before release
+    }
+
     public void updateSelectedContractor() {
+        System.out.println("update");
+
+        Contractor selectedContractor = (Contractor) contractorsTable.getSelectionModel().getSelectedItem();
+        if (selectedContractor != null) {
+
+            updateContractorFromForm(selectedContractor);
+            try {
+                model.updateContractor(selectedContractor);
+                refreshTable();
+                contractorsTable.getSelectionModel().select(selectedContractor);
+            } catch (Exception e) {
+                if (e instanceof NotFoundException) {
+                    AlertLauncher.showAndWait(Alert.AlertType.ERROR, "Błąd", "Nie można zaktualizować kontrahenta.", "Nie znalaziono kontrahenta. Mógł zostać usunięty z bazy.");
+                } else {
+                    AlertLauncher.showAndWait(Alert.AlertType.ERROR, "Błąd", "Nie można zaktualizować kontrahenta.", "Nieznany błąd.");
+                    e.printStackTrace();
+                    System.out.println(e.getClass().getSimpleName());
+                }
+                refreshTable();
+            }
+        }
+    }
+
+    private void updateContractorFromForm(Contractor contractor) {
+        contractor.setContractorName(contractorName.getText());
+        contractor.setFirstName(firstName.getText());
+        contractor.setLastName(lastName.getText());
+        contractor.setPhone(phone.getText());
+        contractor.setEmail(email.getText());
+        contractor.setPesel(pesel.getText());
+        contractor.setNip(nip.getText());
+        contractor.setStreet(street.getText());
+        contractor.setCity(city.getText());
+    }
+
+    private boolean validFormMaxLength() {
+
+        if ((firstName.getText() == null || firstName.getText().length() <= MAX_TEXT_FIELD_LENGTH) &&
+                (lastName.getText() == null || lastName.getText().length() <= MAX_TEXT_FIELD_LENGTH) &&
+                (contractorName.getText() == null || contractorName.getText().length() <= MAX_COMPANY_NAME_LENGTH) &&
+                (city.getText() == null || city.getText().length() <= MAX_TEXT_FIELD_LENGTH) &&
+                (street.getText() == null || street.getText().length() <= MAX_TEXT_FIELD_LENGTH) &&
+                (email.getText() == null || email.getText().length() <= MAX_TEXT_FIELD_LENGTH) &&
+                (phone.getText() == null || phone.getText().length() <= MAX_TEXT_FIELD_LENGTH)
+                ) {
+            return true;
+        }
+        return false;
+    }
+
+    public void deleteContractor() {
+    }
+
+    public void openEmailClient() {
+    }
+
+    public void showDocs() {
+    }
+
+
+    public void saveContractor() {
+        boolean formLengthValid = validFormMaxLength();
+        boolean peselValid = true;//PeselValidator.check(pesel.getText());
+        boolean nipValid = true;
+        if (formLengthValid && peselValid && nipValid) {
+
+            if (formLengthValid && nipValid && peselValid) {
+
+                if (contractorEditing) {
+                    updateSelectedContractor();
+                    contractorEditing = false;
+                }
+
+                setEditMode(false);
+                setFormActive(false);
+
+            } else {
+                if (!formLengthValid) {
+                    AlertLauncher.showAndWait(Alert.AlertType.ERROR, "Błąd", null, "Wprowadzony tekst jest za długi.\n" +
+                            "Maksymalna długość nazwy firmy: 50 znaków.\nPozostałe pola: 25 znaków");
+                }
+                if (!peselValid) {
+                    AlertLauncher.showAndWait(Alert.AlertType.ERROR, "Błąd", null, "Wprowadzony numer PESEL jest niepoprawny.");
+                } else {
+                    AlertLauncher.showAndWait(Alert.AlertType.ERROR, "Błąd", null, "Wprowadzony numer NIP jest niepoprawny.");
+                }//todo: enum with alert messages
+            }
+        }
     }
 
     public void beginContractorAdding() {
+        contractorAdding = true;
+        setEditMode(true);
+        setFormActive(true);
+        contractorsTable.getSelectionModel().select(contractorsTable.getSelectionModel().getSelectedItem());
+    }
+
+    public void beginContractorEditing() {
+        contractorEditing = true;
+        setEditMode(true);
+        setFormActive(true);
+        contractorsTable.getSelectionModel().select(contractorsTable.getSelectionModel().getSelectedItem());
+    }
+
+    public void cancelContractorAddOrEdit() {
+        contractorAdding = false;
+        contractorEditing = false;
+        setEditMode(false);
+        setFormActive(false);
+        updateForm();
+    }
+
+    private void setFormActive(boolean active) {
+        for (Node n : form.getChildren()) {
+            if (n instanceof ComboBox) {
+                n.setDisable(!active);
+            } else if (n instanceof TextField) {
+                ((TextField) n).setEditable(active);
+            }
+        }
+    }
+
+    void setEditMode(boolean editMode) {
+        addButton.setDisable(editMode);
+        editButton.setDisable(editMode);
+        saveButton.setDisable(!editMode);
+        cancelButton.setDisable(!editMode);
+        deleteButton.setDisable(editMode);
     }
 }
--- src/main/java/magazynier/entities/Contractor.java
+++ src/main/java/magazynier/entities/Contractor.java
@@ -113,4 +113,44 @@ public class Contractor implements Indexed {
     public void setId(Integer id) {
         this.id = id;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Contractor)) return false;
+
+        Contractor that = (Contractor) o;
+
+        if (contractorName != null ? !contractorName.equals(that.contractorName) : that.contractorName != null)
+            return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(that.lastName) : that.lastName != null) return false;
+        if (city != null ? !city.equals(that.city) : that.city != null) return false;
+        if (street != null ? !street.equals(that.street) : that.street != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (phone != null ? !phone.equals(that.phone) : that.phone != null) return false;
+        if (contractorType != null ? !contractorType.equals(that.contractorType) : that.contractorType != null)
+            return false;
+        if (entityType != null ? !entityType.equals(that.entityType) : that.entityType != null) return false;
+        if (nip != null ? !nip.equals(that.nip) : that.nip != null) return false;
+        if (pesel != null ? !pesel.equals(that.pesel) : that.pesel != null) return false;
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = contractorName != null ? contractorName.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + (street != null ? street.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        result = 31 * result + (contractorType != null ? contractorType.hashCode() : 0);
+        result = 31 * result + (entityType != null ? entityType.hashCode() : 0);
+        result = 31 * result + (nip != null ? nip.hashCode() : 0);
+        result = 31 * result + (pesel != null ? pesel.hashCode() : 0);
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        return result;
+    }
 }
