d0b43546bd26c0b78cd6c974387b0be2a1088ce1
Potat0x <Potat0x@users.noreply.github.com>
2018-05-05 00:32:08 +0200

    Add EAN validator

---
+++ src/main/java/magazynier/utils/EanValidator.java
@@ -0,0 +1,33 @@
+package magazynier.utils;
+
+public class EanValidator {
+    public static boolean check(String pesel) {
+
+        final int EAN_LEN = 13;
+
+        if (pesel != null && pesel.length() == EAN_LEN) {
+
+            int mtprs[] = {1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3};
+
+            int[] digits = new int[EAN_LEN];
+            for (int i = 0; i < digits.length; i++) {
+                char c = pesel.charAt(i);
+                if (Character.isDigit(c)) {
+                    digits[i] = Character.getNumericValue(c);
+                } else {
+                    return false;
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < pesel.length() - 1; i++) {
+                sum += digits[i] * mtprs[i];
+            }
+
+            if ((10 - (sum % 10)) % 10 == Character.getNumericValue(pesel.charAt(pesel.length() - 1)))
+                return true;
+            else return false;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/magazynier/EanTest.java
@@ -0,0 +1,29 @@
+package magazynier;
+
+import magazynier.utils.EanValidator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class EanTest {
+    @Test
+    public void testCorrectEans() {
+        String[] correctEans = {"8801643134204", "5908267910900", "5908267922644", "4719331301798", "0730143308366",
+                "5032037095693", "5032037087131", "0718037856780"};
+
+        for (String ean : correctEans) {
+            assertTrue(EanValidator.check(ean));
+        }
+    }
+
+    @Test
+    public void testIncorrectEans() {
+        String[] invalidEans = {"", "a", "123", "8801623134204", "3908264910900", "5905267922644", "4712931301798",
+                "0730143302346", "07301433308366", "073014330836", "50320370871311", "3032037095653"};
+
+        for (String ean : invalidEans) {
+            assertFalse(EanValidator.check(ean));
+        }
+    }
+}
