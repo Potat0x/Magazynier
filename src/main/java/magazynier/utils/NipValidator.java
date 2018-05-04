31685aa23bbced66b79717b79da6b23b469b343c
Potat0x <Potat0x@users.noreply.github.com>
2018-05-04 20:14:06 +0200

    Add NIP validator

---
+++ src/main/java/magazynier/utils/NipValidator.java
@@ -0,0 +1,34 @@
+package magazynier.utils;
+
+public class NipValidator {
+
+    public static boolean check(String nip) {
+
+        final int NIP_LEN = 10;
+        nip = nip.replaceAll("[- ]", "");
+        if (nip != null && nip.length() == NIP_LEN) {
+
+            int mtprs[] = {6, 5, 7, 2, 3, 4, 5, 6, 7};
+
+            int[] digits = new int[NIP_LEN];
+            for (int i = 0; i < digits.length; i++) {
+                char c = nip.charAt(i);
+                if (Character.isDigit(nip.charAt(i))) {
+                    digits[i] = Character.getNumericValue(c);
+                } else {
+                    return false;
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < nip.length() - 1; i++) {
+                sum += digits[i] * mtprs[i];
+            }
+
+            if ((sum % 11) == Character.getNumericValue(nip.charAt(nip.length() - 1)))
+                return true;
+            else return false;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/magazynier/NipTest.java
@@ -0,0 +1,29 @@
+package magazynier;
+
+import magazynier.utils.NipValidator;
+import magazynier.utils.PeselValidator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class NipTest {
+    @Test
+    public void testCorrectNips() {
+        String[] correctNips = {"768-000-24-66", "123-456-32-18", "9451972201", "9492107026", "526 - 10 - 44 - 039", "5213415590"};
+
+        for (String nip : correctNips) {
+            assertTrue(NipValidator.check(nip));
+        }
+    }
+
+    @Test
+    public void testIncorrectNips() {
+        String[] invalidNips = {"", "a", "123", " ", "--", "328945239848879", "90190515836", "768-200-24-66", "123-452-12-18", "2461972201",
+                "9492107526", "526 - 18 - 44 - 039", "213415590", "590115590", "20190315833"};
+
+        for (String nip : invalidNips) {
+            assertFalse(NipValidator.check(nip));
+        }
+    }
+}
