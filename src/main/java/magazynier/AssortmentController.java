3f51211fd2c073015ade5cee67867fd44e65da6e
Potat0x <Potat0x@users.noreply.github.com>
2018-05-05 16:10:53 +0200

    Change AssortmentController and add item window

--- src/main/java/magazynier/AssortmentController.java
+++ src/main/java/magazynier/AssortmentController.java
@@ -1,10 +1,48 @@
 package magazynier;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AssortmentController {
+
     @FXML
     public void initialize() {
         System.out.println("AssortmentController initialize");
+    }
+
+    public AssortmentController() {
+        System.out.println("AssortmentController constr");
+    }
+
+    public void addItem() {
+        showItemWindow("Dodawanie towaru");
+    }
+
+    public void editItem() {
+        showItemWindow("Edytowanie towaru");
+    }
+
+    public void deleteItem() {
+    }
+
+    private void showItemWindow(String title) {
+        FXMLLoader itemStageLoader = new FXMLLoader(getClass().getResource("/fxml/item_window.fxml"));
+        ItemController ic = new ItemController(title);
+        itemStageLoader.setController(ic);
+        Stage itemStage = new Stage();
+        itemStage.setTitle("Dodawanie towaru");
+        Parent parent = null;
+        try {
+            parent = itemStageLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        itemStage.setScene(new Scene(parent));
+        itemStage.show();
     }
 }
---
+++ src/main/java/magazynier/ItemController.java
@@ -0,0 +1,40 @@
+package magazynier;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+public class ItemController {
+    public TextField name;
+    public TextField ean;
+    public TextField itemModelNumber;
+    public TextField measurementUnit;
+    public TextField price;
+    public TextField desiredQuantity;
+    public ComboBox vatRate;
+    public TextArea description;
+    public Button saveButton;
+    public Button cancelButton;
+    public Label titleLabel;
+
+    public ItemController(String param) {
+        this.param = param;
+    }
+
+    private String param;
+
+
+    @FXML
+    public void initialize() {
+        System.out.println("ItemController initialize");
+        titleLabel.setText(param);
+    }
+
+    public void saveItem() {
+    }
+
+    public void cancelItemAddOrEdit() {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+}
