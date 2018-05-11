d0eb73fc2f99abe2c270dcdde035b577e25c9ede
Potat0x <Potat0x@users.noreply.github.com>
2018-05-11 22:58:15 +0200

    Fix existence verification bad code

--- src/main/java/magazynier/DAO.java
+++ src/main/java/magazynier/DAO.java
@@ -4,6 +4,7 @@ import magazynier.utils.Indexed;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.ArrayList;
 
@@ -28,7 +29,6 @@ public class DAO {
             Transaction tr = session.beginTransaction();
             session.save(object);
             tr.commit();
-            session.close();
         } catch (HibernateException e) {
             e.printStackTrace();
         }
@@ -36,38 +36,33 @@ public class DAO {
 
     public static void update(Indexed object) throws RowNotFoundException {
 
-        if (checkIfExistsById(object.getClass(), object.getId())) {
-            try (Session session = HibernateSessionFactory.openSession()) {
+        try (Session session = HibernateSessionFactory.openSession()) {
+            if (checkIfExists(session, object)) {
                 Transaction tr = session.beginTransaction();
                 session.update(object);
                 tr.commit();
-                session.close();
-            } catch (HibernateException e) {
-                e.printStackTrace();
+            } else {
+                throw new RowNotFoundException(object.getClass() + " object:" + object + " does not exist in database.");
             }
-        } else {
-            throw new RowNotFoundException(object.getClass() + " object:" + object + " does not exist in database.");
         }
     }
 
     public static void delete(Indexed object) throws RowNotFoundException {
 
-        if (checkIfExistsById(object.getClass(), object.getId())) {
-            Session session = HibernateSessionFactory.openSession();
+        try (Session session = HibernateSessionFactory.openSession()) {
             Transaction tr = session.beginTransaction();
-            session.delete(object);
+            if (checkIfExists(session, object)) {
+                session.delete(object);
+            } else {
+                throw new RowNotFoundException(object.getClass() + " object:" + object + " does not exist in database.");
+            }
             tr.commit();
-            session.close();
-        } else {
-            throw new RowNotFoundException(object.getClass() + " object:" + object + " does not exist in database.");
         }
     }
 
-    private static boolean checkIfExistsById(Class c, Integer id) {
-        Object object = new Object();
-        Session session = HibernateSessionFactory.openSession();
-        Object obj = session.get(c, id);
-        session.close();
-        return obj != null;
+    private static boolean checkIfExists(Session session, Indexed object) {
+        Query query = session.createQuery("select 'exists' from " + object.getClass().getSimpleName() + " c where c.id = :id");
+        query.setProperties(object);
+        return query.uniqueResult() != null;
     }
 }
